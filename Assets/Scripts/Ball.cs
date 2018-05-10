5334074e1d899d034a5f477971334053fa72c0c2
lizijun_Home <lizijun_youle@163.com>
2018-05-11 00:07:08 +0800

    采用Lerp方法编写小球跳跃动画

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,13 +5,39 @@ using MyCommonTools;
 
 public class Ball : MonoSingleton<Ball>
 {
-    // Use this for initialization
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private Vector3 pos;
+    private bool isArrived;
+    private float upSpeed = 0.18f;
+    private float downSpeed = 0.28f;
+    private float speed;
+
     private void Start()
     {
+        startPos = transform.position;
+        endPos = startPos + new Vector3(0, 4, 0);
+        pos = endPos;
+        speed = upSpeed;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        JumpUP();
+    }
+
+    private void JumpUP()
+    {
+        if (Vector3.Distance(transform.position, pos) > 0.05f)
+        {
+            transform.position = Vector3.Lerp(transform.position, pos, speed);
+        }
+        else
+        {
+            isArrived = !isArrived;
+            pos = isArrived ? startPos : endPos;
+            speed = isArrived ? downSpeed : upSpeed;
+        }
     }
 }
